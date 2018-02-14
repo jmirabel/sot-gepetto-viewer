b8a7ced040866ae099710611e67a4855ceb36939
Joseph Mirabel <jmirabel@laas.fr>
2018-02-14 17:39:38 +0100

    Initial commit

---
+++ src/command_execution.py
@@ -0,0 +1,119 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Dorian Scholz
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import rospy
+
+import dynamic_graph_bridge.srv
+import dynamic_graph_bridge_msgs.srv
+
+# rospy.init_node ("sot_gepetto_viewer_plugin", anonymous=True)
+
+class CommandExecution(object):
+    # _localsGetter  = None
+    # _globalsGetter = None
+    # _locals  = None
+    # _globals = None
+    _local = False
+
+    def __init__(self):
+        self._client = rospy.ServiceProxy(
+            'run_command', dynamic_graph_bridge_msgs.srv.RunCommand, True)
+        self._locals  = {}
+        self._globals = {}
+
+    # @staticmethod
+    # def localsGetter(g):
+      # CommandExecution._localsGetter = g
+    # @staticmethod
+    # def globalsGetter(g):
+      # CommandExecution._globalsGetter = g
+
+    # @staticmethod
+    # def locals(l):
+      # CommandExecution._locals = l
+    # @staticmethod
+    # def globals(g):
+      # CommandExecution._globals = g
+    @staticmethod
+    def dgIsLocal():
+      CommandExecution._local = True
+
+    def run(self, code, retValue = True):
+        # if CommandExecution._localsGetter is not None:
+        # if CommandExecution._locals is not None:
+        if CommandExecution._local:
+          return self.runLocalCode (code, retValue)
+        else:
+          return self.runRemoteCode (code, retValue, True)
+
+    def runLocalCode(self, code, retValue):
+        # print ("Local")
+        # l = CommandExecution._localsGetter()
+        # g = CommandExecution._globalsGetter() if CommandExecution._globalsGetter is not None else {}
+        # l = CommandExecution._locals
+        # g = CommandExecution._globals
+        l = self._locals
+        g = self._globals
+        if retValue:
+          exec("_returnValue = " + code, g, l)
+          return eval("_returnValue", g, l)
+        else:
+          exec(code, g, l)
+
+    def runRemoteCode(self, code, retValue, retry = True):
+        # print ("Remote" + str(retValue))
+        try:
+            if not self._client:
+                if not retry:
+                    print("Connection to remote server lost. Reconnecting...")
+                self._client = rospy.ServiceProxy(
+                    'run_command', dynamic_graph_bridge_msgs.srv.RunCommand, True)
+            response = self._client(str(code))
+            if response.standardoutput != "":
+                print(response.standardoutput[:-1])
+            if response.standarderror != "":
+                print(response.standarderror[:-1])
+            elif response.result != "None":
+                if not retValue: return
+                res = eval(response.result)
+                # print(response.result)
+                return res
+        except rospy.ServiceException, e:
+            print("Connection to remote server lost. Reconnecting...")
+            self._client = rospy.ServiceProxy(
+                'run_command', dynamic_graph_bridge_msgs.srv.RunCommand, True)
+            if retry:
+                self.runRemoteCode(code, False)
+            else:
+                print("Failed to connect. Is Stack of Tasks running?")
+                raise IOError("Failed to connect. Is Stack of Tasks running?")
---
+++ src/sot_plugin.py
@@ -0,0 +1,168 @@
+from PythonQt import QGraphViz, QtGui, Qt
+from command_execution import CommandExecution
+import rospy
+
+class Graph:
+    def __init__(self, plugin):
+        self.plugin = plugin
+        self.graph = QGraphViz.QGVScene("graph")
+        self.view = QtGui.QGraphicsView (self.graph)
+        self.layoutShouldBeFreed = False
+        self.graph.connect("nodeMouseRelease(QGVNode*)", self.updateLayout)
+        self.graph.connect("nodeContextMenu(QGVNode*)", self._nodeContextMenu)
+
+        self.typeCallbacks = {
+                "Task": ( self._nodeEntityTask, self._edgeEntityTask ),
+                "SOT" : ( self._nodeEntitySOT , self._edgeEntitySOT  )
+                }
+
+        self.initCmd()
+        self.createAllGraph()
+
+    def clear (self):
+        if self.layoutShouldBeFreed:
+            self.graph.freeLayout()
+            self.layoutShouldBeFreed = False
+        self.nodes = {}
+        self.types = {}
+        self.edges = {}
+        self.subgraphs = {}
+        self.graph.clear()
+
+
+    def initLayout (self):
+        if self.layoutShouldBeFreed:
+            self.graph.freeLayout()
+        self.graph.applyLayout("dot")
+        self.graph.setNodePositionAttribute()
+        self.graph.setGraphAttribute("splines","spline")
+        self.graph.setGraphAttribute("overlap","false")
+        self.graph.setNodeAttribute ("pin","true")
+        self.layoutShouldBeFreed = True
+
+    def updateLayout (self):
+        if self.layoutShouldBeFreed:
+            self.graph.freeLayout()
+        self.graph.applyLayout("nop2")
+        self.layoutShouldBeFreed = True
+
+    def initCmd (self):
+        self.cmd = CommandExecution()
+        self.cmd.run ("import dynamic_graph as dg", False)
+
+    def createAllGraph (self):
+        entities = self.cmd.run ("dg.entity.Entity.entities.keys()")
+        self.clear()
+        for e in entities:
+            etype = self.cmd.run("dg.entity.Entity.entities['"+e+"'].className")
+            self.types[e] = etype
+            if self.typeCallbacks.has_key(etype):
+                self.typeCallbacks[etype][0] (e)
+            else:
+                self._nodeEntity(e)
+        for e in entities:
+            etype = self.types[e]
+            if self.typeCallbacks.has_key(etype):
+                self.typeCallbacks[etype][1] (e)
+            else:
+                self._edgeEntitySignals (e)
+        self.initLayout()
+
+    def createGraphBackwardFromEntity (self, e):
+        ok = self.cmd.run("dg.entity.Entity.entities.has_key('"+e+"')")
+        if not ok:
+            raise ValueError ("Entity " + e + " does not exist")
+        self.clear()
+        self._createGraphBackwardFromEntity (e)
+        self.initLayout()
+
+    def _createGraphBackwardFromEntity (self, e):
+        etype = self.cmd.run("dg.entity.Entity.entities['"+e+"'].className")
+        self.types[e] = etype
+        if self.typeCallbacks.has_key(etype):
+            self.typeCallbacks[etype][0] (e)
+            self.typeCallbacks[etype][1] (e)
+        else:
+            self._nodeEntity(e)
+            self._edgeEntitySignals (e)
+
+    def _nodeEntitySOT(self, s):
+        subgraph = self.graph.addSubGraph("Tasks in " + s, True) # True means "cluster"
+        # subgraph.setAttribute("rank", "same")
+        # subgraph.setAttribute("rank", "max")
+        # subgraph.setAttribute("rankdir", "LR")
+        subgraph.setAttribute("ranksep", "0.1")
+        # print self.cmd.run("dg.entity.Entity.entities['"+s+"'].display()")
+        tasks = eval(self.cmd.run("dg.entity.Entity.entities['"+s+"'].list()"))
+        nodes = []
+        for i, t in enumerate(tasks):
+            node = subgraph.addNode (str(i))
+            #TODO set properties
+            nodes.append(node)
+        # node = self.graph.addNode(s)
+        node = subgraph.addNode(s)
+        # node.setAttribute("rank", "max")
+        self.nodes[s] = node
+        for prev, node in zip(nodes,nodes[1:] + [node,]):
+            edge = self.graph.addEdge (prev,node)
+            # edge.setAttribute("constraint","false")
+            #TODO set properties
+        self.subgraphs[s] = (subgraph, tasks, nodes)
+        # self._nodeEntity(s)
+
+    def _edgeEntitySOT(self, s):
+        subgraph, tasks, nodes = self.subgraphs[s]
+        for t,n in zip(tasks, nodes):
+            if not self.nodes.has_key(t):
+                self._createGraphBackwardFromEntity(t)
+            self.graph.addEdge (self.nodes[t], n)
+
+    def _nodeEntityTask(self, t):
+        self._nodeEntity(t)
+
+    def _edgeEntityTask(self, t):
+        # Build features list
+        features = eval(self.cmd.run("dg.entity.Entity.entities['"+t+"'].list()"))
+        for f in features:
+            if not self.nodes.has_key(f):
+                self._createGraphBackwardFromEntity(f)
+            edge = self.graph.addEdge (self.nodes[f], self.nodes[t])
+            # TODO set edge properties
+            edge.setAttribute("color", "red")
+        self._edgeEntitySignals (t)
+        pass
+
+    def _nodeEntity(self, e):
+        self.nodes[e] = self.graph.addNode (e)
+
+    def _edgeEntitySignals(self, e):
+        signals = self.cmd.run("[ s.name for s in dg.entity.Entity.entities['"+e+"'].signals() ]")
+        for s in signals:
+            ss = s.split("::")
+            if ss[1].startswith("in"):
+                plugged = self.cmd.run("dg.entity.Entity.entities['"+e+"'].signal('"+ss[-1]+"').isPlugged()")
+                other_s = self.cmd.run("dg.entity.Entity.entities['"+e+"'].signal('"+ss[-1]+"').getPlugged().name if dg.entity.Entity.entities['"+e+"'].signal('"+ss[-1]+"').isPlugged() else None")
+                if other_s is not None and s != other_s:
+                    idx = other_s.index('(')+1
+                    other_e = other_s[idx:other_s.index(')',idx)]
+                    if not self.nodes.has_key(other_e):
+                        self._createGraphBackwardFromEntity(other_e)
+                    self.edges[s] = self.graph.addEdge (self.nodes[other_e], self.nodes[e], ss[2])
+            elif ss[1].startswith("out"):
+                pass
+            else:
+                print "unknown", s
+
+    def _nodeContextMenu (self, node):
+        e = node.getAttribute("label")
+        if self.nodes.has_key(e):
+            print e
+
+class Plugin(QtGui.QDockWidget):
+    def __init__(self, main):
+        super(Plugin, self).__init__ ("Stack Of Tasks plugin", main)
+        self.setObjectName("Stack Of Tasks plugin")
+        self.main = main
+        self.graph = Graph (self)
+
+        self.setWidget (self.graph.view)
